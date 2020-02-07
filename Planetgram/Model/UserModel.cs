573261f7b7bc9d300bf9e3f44823fb8c88eb980a
Ameer <obssesionx@gmail.com>
2020-02-07 21:39:19 +0200

    Initialization

---
+++ Planetgram/Model/UserModel.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Planetgram.Model
+{
+    public class UserModel{ }
+    public class User : INotifyPropertyChanged
+    {
+        private string userName;
+        private string userPassword;
+        private int userFollowers;
+        private byte[] userImage;
+
+        public string UserName
+        {
+            get
+            {
+                return userName;
+            }
+
+            set
+            {
+                if (userName != value)
+                {
+                    userName = value;
+                    RaisePropertyChanged("UserName");
+                }
+            }
+        }
+
+        public string UserPassword
+        {
+            get { return userPassword; }
+
+            set
+            {
+                if (userPassword != value)
+                {
+                    userPassword = value;
+                    RaisePropertyChanged("UserPassword");
+                }
+            }
+        }
+        public int UserFollowers
+        {
+            get { return userFollowers; }
+
+            set
+            {
+                if (userFollowers != value)
+                {
+                    userFollowers = value;
+                    RaisePropertyChanged("UserFollowers");
+                }
+            }
+        }
+        public byte[] UserImage
+        {
+            get { return userImage; }
+
+            set
+            {
+                if (userImage != value)
+                {
+                    userImage = value;
+                    RaisePropertyChanged("UserImage");
+                }
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void RaisePropertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+    }
+
+}
---
+++ Planetgram/Services/InstagramApiService.cs
@@ -0,0 +1,35 @@
+﻿using InstaSharper.API;
+using InstaSharper.API.Builder;
+using InstaSharper.Classes;
+using InstaSharper.Logger;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Planetgram.Services
+{
+    public class InstagramApiService
+    {
+        private static UserSessionData user;
+        private static IInstaApi api;
+
+        public InstagramApiService(string username = "" , string password = "")
+        {
+            user = new UserSessionData();
+            user.UserName = username;
+            user.Password = password;
+            InitApi();
+        
+        }
+        private void InitApi()
+        {
+            api = InstaApiBuilder.CreateBuilder()
+           .SetUser(user)
+           .UseLogger(new DebugLogger(LogLevel.Exceptions))
+           //.SetRequestDelay(TimeSpan.FromSeconds(10))
+           .Build();
+        }
+    }
+}

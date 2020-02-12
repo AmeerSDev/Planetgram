928ee9b8aa1db4f269f6d0ea8f0633777c340e8d
Ameer <obssesionx@gmail.com>
2020-02-12 17:17:35 +0200

    Added a UserPanel for searching

--- Planetgram/MainWindowViewModel.cs
+++ Planetgram/MainWindowViewModel.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Planetgram.Views
+namespace Planetgram
 {
-    class UserView
+    public class MainWindowViewModel
     {
     }
 }

420e4213839215f219848def47d96ae12ac0673b
Ameer <obssesionx@gmail.com>
2020-02-20 19:10:22 +0200

    Added Scraper class + required packages

---
+++ Planetgram/Handler/Scraper.cs
@@ -0,0 +1,58 @@
+﻿using HtmlAgilityPack;
+using Newtonsoft.Json.Linq;
+using Planetgram.Model;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Planetgram.Handler
+{
+    public static class Scraper
+    {
+        public static async Task<User> ScrapeInstagram(string url)
+        {
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetAsync(url);
+                if (response.IsSuccessStatusCode)
+                {
+                    // create html document
+                    var htmlBody = await response.Content.ReadAsStringAsync();
+                    var htmlDocument = new HtmlDocument();
+                    htmlDocument.LoadHtml(htmlBody);
+
+                    // select script tags
+                    var scripts = htmlDocument.DocumentNode.SelectNodes("/html/body/script");
+
+                    // preprocess result
+                    var uselessString = "window._sharedData = ";
+                    var scriptInnerText = scripts[0].InnerText
+                        .Substring(uselessString.Length)
+                        .Replace(";", "");
+
+                    // serialize objects and fetch the user data
+                    dynamic jsonStuff = JObject.Parse(scriptInnerText);
+                    dynamic userProfile = jsonStuff["entry_data"]["ProfilePage"][0]["graphql"]["user"];
+
+                    // create an InstagramUser
+                    var instagramUser = new User
+                    {
+                        UserName = userProfile.full_name,
+                        UserFollowers = userProfile.edge_followed_by.count,
+                        UserFollowings = userProfile.edge_follow.count
+                    };
+                    Debug.Print($"Name: {instagramUser.UserName},Followers: {instagramUser.UserFollowers} , Following: {instagramUser.UserFollowings}");
+                    return instagramUser;
+                }
+                else
+                {
+                    throw new Exception($"Something wrong happened {response.StatusCode} - {response.ReasonPhrase} - {response.RequestMessage}");
+                }
+            }
+        }
+    }
+}

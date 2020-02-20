390915c38e71d29b03a1c5134248ac2503307887
Ameer <obssesionx@gmail.com>
2020-02-20 19:06:01 +0200

    UserViewModel basic logic(untested)

--- Planetgram/ViewModel/UserViewModel.cs
+++ Planetgram/ViewModel/UserViewModel.cs
@@ -1,6 +1,8 @@
-﻿using Planetgram.Model;
+﻿using Planetgram.Handler;
+using Planetgram.Model;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,8 +11,57 @@ namespace Planetgram.ViewModel
 {
     public class UserViewModel
     {
+        private User igUser = new User();
+        private ObservableCollection<User> _users = new ObservableCollection<User>();
+        public ICommandImp ScrapeCommand { get; set; }
+
+        public ObservableCollection<User> Users
+        {
+            get { return _users; }
+            set { _users = value; }
+        }
         public UserViewModel()
         {
+                 ScrapeCommand = new ICommandImp(OnScrape,CanScrape);
+            //   DisplayUser();
+        }
+        public User IGUser
+        {
+            get
+            {
+                return igUser;
+            }
+
+            set
+            {
+                igUser = value;
+                ScrapeCommand.RaiseCanExecuteChanged();
+            }
+        }
+
+        public void DisplayUser()
+        {
+
+            if (_users != null)
+            {
+                _users[0].UserName = IGUser.UserName;
+                _users[0].UserFollowers = IGUser.UserFollowers;
+                _users[0].UserFollowings = IGUser.UserFollowings;
+            }
+            else
+                return;
+        }
+        private async void OnScrape()
+        {
+           igUser = await Scraper.ScrapeInstagram($"https://www.instagram.com//{igUser.UserName}/");
+        }
+
+        private bool CanScrape()
+        {
+            if (igUser != null)
+                return true;
+            return false;
+
         }
     }
 }
